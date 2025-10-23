e4d93f980ba2930dc3bcc426f7f07e6128667d41
loicduong <mikhailcolt@gmail.com>
2025-10-23 10:31:23 +0700

    fix: wrong setupLayouts type

--- client.d.ts
+++ client.d.ts
@@ -2,5 +2,5 @@ declare module 'virtual:generated-layouts' {
   import type { Router, RouteRecordRaw } from 'vue-router'
   // need any here due to different types for vue-router versions
   export function createGetRoutes(router: Router | any, withLayout?: boolean): () => RouteRecordRaw[]
-  export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
+  export function setupLayouts(routes: readonly RouteRecordRaw[]): RouteRecordRaw[]
 }
